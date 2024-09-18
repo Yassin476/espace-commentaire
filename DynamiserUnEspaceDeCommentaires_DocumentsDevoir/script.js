bf798520ac6d2d45bcff4c1d74bb77619c713727
Yassin <Yassman9477@gmail.com>
2024-09-18 22:03:53 +0200

    script.js

---
+++ DynamiserUnEspaceDeCommentaires_DocumentsDevoir/script.js
@@ -0,0 +1,40 @@
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    const form = document.querySelector('form');
+    const commentList = document.getElementById('comment-list');
+    const errorMessage = document.getElementById('error-message');
+    console.log(form);
+
+    form.addEventListener('submit', function (event ) {
+         event.preventDefault();
+    
+
+        const firstName = document.getElementById('first-name').value.trim();
+        const lastName = document.getElementById('last-name').value.trim();
+        const message = document.getElementById('message').value.trim();
+
+        if ( !firstName || !lastName || !message) {
+            errorMessage.style.display = 'block';
+            return;
+        } else {
+            errorMessage.style.display = 'none';
+        }
+
+        const newComment = document.createElement('div');
+        newComment.classList.add('flex', 'space-x-4', 'text-sm', 'text-gray-500');
+        newComment.innerHTML = ` 
+            <div class="flex-1 py-10 border-t border-gray-200">
+                <h3 class="font-medium text-gray-900">${firstName} ${lastName}</h3>
+                <div class="prose prose-sm mt-4 max-w-none text-gray-500">
+                    <p>${message}</p>
+                </div>
+            </div>
+        `;
+
+        commentList.appendChild(newComment);
+
+        form.reset();
+    });
+});
+
